86ce2543cf75b65bfd99f4031d0dce5e492243aa
Monsur Khan <monsur.khan@gmail.com>
2019-10-22 20:16:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,15 +23,15 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array
+  create_an_array[]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  create_an_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+ create_an_array[-1] 
 end
 
 def update_element_from_index(array, index_number, element)
